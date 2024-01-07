9e9a08e9a9255c18ea3956dcf1ee8e451f752da2
sandro-git <s.dasneves79@gmail.com>
2024-01-07 16:46:39 +0100

    start testing full calendar

--- nuxt3/app.vue
+++ nuxt3/app.vue
@@ -1,37 +1,37 @@
 <template>
   <div class='app'>
-    <FullCalendar :options='calendarOptions' />
+    <FullCalendar :options=calendarOptions />
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import {  ref } from "vue";
 import FullCalendar from '@fullcalendar/vue3'
 import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import  dayGridPlugin from "@fullcalendar/daygrid";
+import multiMonthPlugin from "@fullcalendar/multimonth";
 
-export default {
-  components: {
-    FullCalendar
+const calendarOptions =ref( {
+  plugins: [interactionPlugin, timeGridPlugin ,dayGridPlugin,multiMonthPlugin],
+  initialView: 'timeGridWeek',
+  nowIndicator: true,
+  headerToolbar:{
+    letf:"prev,next",
+    center:"title",
+    right:"timeGridWeek,timeGridDay"
   },
-  data() {
-    return {
-      calendarOptions: {
-        plugins: [interactionPlugin, timeGridPlugin],
-        initialView: 'timeGridWeek',
-        nowIndicator: true,
-        editable: true,
-        initialEvents: [
-          { title: 'nice event', start: new Date() }
-        ]
-      }
-    }
-  }
-}
+  editable: true,
+  initialEvents: [
+    { title: 'nice event', start: new Date() }
+  ]
+})
+
 </script>
 
 <style scoped>
   .app {
     font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-    font-size: 14px;
+    font-size: 16px;
   }
 </style>
